4ebcc097fb11c46521748e078c759859acb6fc7b
nick <515122644@qq.com>
2017-01-02 22:56:47 +0800

    使用深度优先遍历算法解决妖怪与和尚过河问题

--- src/main/java/org/jinlong/study/algorithm/Util.java
+++ src/main/java/org/jinlong/study/algorithm/Util.java
@@ -6,6 +6,11 @@ import java.util.List;
  * Created by nick on 02/01/2017.
  */
 public class Util {
+    /**
+     * Print items to console.
+     * @param list
+     * @param <T>
+     */
     public static <T> void  printListItems(List<T> list) {
         if (list == null) {
             return;
